ff497eb98a7ef629a56ed2ae375b602fac1e6d98
Matheus Junior <bonecodoido339@gmail.com>
2025-03-19 14:49:00 -0300

    feat: Create Model user And Table

--- BackEnd/SalaoDeBelezaBackEnd/database/migrations/2025_03_19_173113_create_users.php
+++ BackEnd/SalaoDeBelezaBackEnd/database/migrations/2025_03_19_173113_create_users.php
@@ -13,12 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('username')->nullable();
+            $table->string('usercpf')->nullable();
+            $table->string('useremail')->nullable(); 
+            $table->string('password')->nullable(); 
+            $table->string('userphone')->nullable(); 
+            $table->string('userstatus')->nullable(); 
             $table->timestamps();
+         
         });
     }
 
